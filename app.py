a7f03923a3c082b87830eed2efbc1ab4539d4790
Benjamin Balcinovic <benjamin.balcinovic@bulme.at>
2022-05-31 10:26:39 +0200

    codequalität mit pylama verbessert

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from models import db
-from flask.templating import render_template
 from controllers.index import index_blueprint
 from controllers.laufer import laufer_blueprint
 from controllers.marathonlauf import marathonlauf_blueprint
 from controllers.organisator import organisator_blueprint
 
-import sqlalchemy
 app = Flask(__name__)
 app.secret_key = "VerySecretSecretKey"
 
@@ -15,7 +13,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:rootroot@localhost/Maratho
 
 db.init_app(app)
 
-#hier blueprint registrieren
+# hier blueprint registrieren
 app.register_blueprint(index_blueprint)
 app.register_blueprint(laufer_blueprint)
 app.register_blueprint(marathonlauf_blueprint)
